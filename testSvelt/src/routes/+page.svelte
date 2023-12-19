c650579f0224fdf0c5c5954d2ef23c1a8c9190da
AntoineGiusti <giusti.antoine@gmail.com>
2023-12-19 14:59:11 +0100

    add row and col for home and about

--- testSvelt/src/routes/+page.svelte
+++ testSvelt/src/routes/+page.svelte
@@ -4,9 +4,14 @@
 
 <div class="container">
     <h1>Bienvenue sur mon test</h1>
-
-    <a href="/about">A propos</a>
-    <a href="/contact">Contact</a>
+    <div class="row">
+        <div class="col-6">
+            <a href="/about">A propos</a>
+        </div>
+        <div class="col-6">
+            <a href="/contact">Contact</a>
+        </div>
+    </div>
 </div>
 
 <style>
--- testSvelt/src/routes/about/+page.svelte
+++ testSvelt/src/routes/about/+page.svelte
@@ -4,8 +4,14 @@
 
 <div class="container">
     <h1>A propos</h1>
-    <a href="/">Accueil</a>
-    <a href="/contact">Contact</a>
+    <div class="row">
+        <div class="col-6">
+            <a href="/">Accueil</a>
+        </div>
+        <div class="col-6">
+            <a href="/contact">Contact</a>
+        </div>
+    </div>
 </div>
 
 <style>
